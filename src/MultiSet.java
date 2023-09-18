c5d7fa3988f00cda5c89b2fb475d69ccaa273b86
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-09-18 14:20:46 -0400

    Finished public class MultiSet

---
+++ src/MultiSet.java
@@ -0,0 +1,35 @@
+
+public class MultiSet {
+    /*
+    An abstract class representing the MultiSet ADT, which supports the
+    add, remove, is_empty, count, and contains operations.
+
+    This class itself does not handle how the underlying data is stored,
+    so it just inherits Object.__init__.
+    */
+
+    public boolean add(Object item) {
+        throw new UnsupportedOperationException("Error");
+    }
+
+    public boolean remove(Object item) {
+        throw new UnsupportedOperationException("Error");
+    }
+
+    public boolean contains(Object item) {
+        throw new UnsupportedOperationException("Error");
+    }
+
+    public boolean is_empty() {
+        throw new UnsupportedOperationException("Error");
+    }
+
+    public boolean count(Object item) {
+        throw new UnsupportedOperationException("Error");
+    }
+
+    public boolean size() {
+        throw new UnsupportedOperationException("Error");
+    }
+
+}
