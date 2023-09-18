78c5a2fbb5a10a972e1c926c1e667014a8c97c02
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-09-18 14:44:04 -0400

    treemultiset commit ting

---
+++ src/TreeMultiSet.java
@@ -0,0 +1,34 @@
+
+class TreeMultiSet extends MultiSet {
+    private Tree tree;
+
+    public TreeMultiSet() {
+        this.tree = Tree(null);
+    }
+
+    public boolean add(Object item) {
+        this.tree.insert(item);
+        return true;
+    }
+
+    public void remove(Object item) {
+        this.tree.delete_item(item);
+    }
+
+    public boolean contains(Object item) {
+        return this.tree.contains(item);
+    }
+
+    public boolean isEmpty(){
+        return this.tree.isEmpty();
+    }
+
+    public boolean count(Object item) {
+        return this.tree.count(item);
+    }
+
+    public int size(){
+        return this.tree.length();
+    }
+
+}
